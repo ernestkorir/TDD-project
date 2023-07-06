fe9bb9ee895bd68263f046296268cd25148205e7
Ernest Korir <ernestbore3@gmail.com>
2023-07-06 21:06:36 +0300

    Add Fizzbuzz method

--- solver.rb
+++ solver.rb
@@ -1,14 +1,21 @@
 class Solver
-  def factorial(num)
-    raise ArgumentError, 'Negative integer not allowed' if num.negative?
+  def factorial(n)
+    raise ArgumentError, 'Negative integer not allowed' if n.negative?
 
     return 1 if n.zero?
 
-    (1..num).reduce(:*)
+    (1..n).reduce(:*)
   end
 
   def reverse(word)
     word.reverse
   end
-  
+
+  def fizzbuzz(n)
+    return 'fizzbuzz' if (n % 3).zero? && (n % 5).zero?
+    return 'fizz' if (n % 3).zero?
+    return 'buzz' if (n % 5).zero?
+
+    n.to_s
+  end
 end
