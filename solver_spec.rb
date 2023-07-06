671c0afaa67d22fe790c9d5773c1b6186278e394
Tom Daniel <tomudoh258@gmail.com>
2023-07-06 18:22:45 +0100

    Initailaize test case for solver class

---
+++ solver_spec.rb
@@ -0,0 +1,20 @@
+# solver_spec.rb
+
+require 'rspec'
+require_relative 'solver'
+
+RSpec.describe Solver do
+
+  describe '#factorial' do
+
+  end
+
+  describe '#reverse' do
+
+  end
+
+
+  describe '#fizzbuzz' do
+
+  end
+end
