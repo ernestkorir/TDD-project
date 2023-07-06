31107437fb47cb925ec2b1963a41f4423adfbcee
Tom Daniel <tomudoh258@gmail.com>
2023-07-06 18:53:03 +0100

    Apply Best practices to code

--- solver.rb
+++ solver.rb
@@ -1,10 +1,9 @@
 class Solver
-    def factorial(n)
-      raise ArgumentError, 'Negative integer not allowed' if n.negative?
-  
-      return 1 if n.zero?
-  
-      (1..n).reduce(:*)
-    end
+  def factorial(num)
+    raise ArgumentError, 'Negative integer not allowed' if num.negative?
+
+    return 1 if n.zero?
+
+    (1..num).reduce(:*)
   end
-  
+end
--- solver_spec.rb
+++ solver_spec.rb
@@ -1,30 +1,25 @@
-# solver_spec.rb
-
 require 'rspec'
 require_relative 'solver'
 
 RSpec.describe Solver do
-    # Add tests for the 'factorial' method
-    describe '#factorial' do
-      it 'returns 1 for the factorial of 0' do
-        expect(subject.factorial(0)).to eq(1)
-      end
-  
-      it 'returns the factorial for a positive integer' do
-        expect(subject.factorial(5)).to eq(120)
-      end
-  
-      it 'raises an exception for a negative integer' do
-        expect { subject.factorial(-1) }.to raise_error(ArgumentError)
-      end
+  describe '#factorial' do
+    it 'returns 1 for the factorial of 0' do
+      expect(subject.factorial(0)).to eq(1)
     end
 
-  describe '#reverse' do
+    it 'returns the factorial for a positive integer' do
+      expect(subject.factorial(5)).to eq(120)
+    end
 
+    it 'raises an exception for a negative integer' do
+      expect { subject.factorial(-1) }.to raise_error(ArgumentError)
+    end
+  end
+
+  describe '#reverse' do
   end
 
 
   describe '#fizzbuzz' do
-
   end
 end
