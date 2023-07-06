c416e5f623c1b2c94a53c1a00dfd11a970715233
Ernest Korir <ernestbore3@gmail.com>
2023-07-06 21:02:54 +0300

    Add Fizzbuzz tes

--- solver_spec.rb
+++ solver_spec.rb
@@ -1,5 +1,5 @@
 require 'rspec'
-require_relative 'solver'
+require_relative '../solver'
 
 RSpec.describe Solver do
   describe '#factorial' do
@@ -25,5 +25,20 @@ RSpec.describe Solver do
   end
 
   describe '#fizzbuzz' do
+    it 'returns "fizz" when the number is divisible by 3' do
+      expect(solver.fizzbuzz(9)).to eq('fizz')
+    end
+
+    it 'returns "buzz" when the number is divisible by 5' do
+      expect(solver.fizzbuzz(10)).to eq('buzz')
+    end
+
+    it 'returns "fizzbuzz" when the number is divisible by 3 and 5' do
+      expect(solver.fizzbuzz(15)).to eq('fizzbuzz')
+    end
+
+    it 'returns the number as a string for any other case' do
+      expect(solver.fizzbuzz(7)).to eq('7')
+    end
   end
 end
