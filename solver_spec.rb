5d0e8e3f9927fff4a1f05ca7da14e0af8760df61
Tom Daniel <tomudoh258@gmail.com>
2023-07-06 18:27:54 +0100

    Add test case for factorial

--- solver_spec.rb
+++ solver_spec.rb
@@ -4,10 +4,20 @@ require 'rspec'
 require_relative 'solver'
 
 RSpec.describe Solver do
-
-  describe '#factorial' do
-
-  end
+    # Add tests for the 'factorial' method
+    describe '#factorial' do
+      it 'returns 1 for the factorial of 0' do
+        expect(subject.factorial(0)).to eq(1)
+      end
+  
+      it 'returns the factorial for a positive integer' do
+        expect(subject.factorial(5)).to eq(120)
+      end
+  
+      it 'raises an exception for a negative integer' do
+        expect { subject.factorial(-1) }.to raise_error(ArgumentError)
+      end
+    end
 
   describe '#reverse' do
 
